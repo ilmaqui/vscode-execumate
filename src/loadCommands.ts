6b0e91753050af4e8437d014929f8154466328c5
ilmaqui <78042340+ilmaqui@users.noreply.github.com>
2024-04-25 12:50:53 +0200

    Update version to 0.1.0 and add installation instructions to README.md

--- src/loadCommands.ts
+++
@@ -1,6 +0,0 @@
-import { readCommandsFromFile } from "./fileOperations";
-import { CommandType } from "./models/command-type";
-import { TerminalDataProvider } from "./models/terminal-data-provider";
-import { State } from "./models/state";
-import * as vscode from "vscode";
-import * as path from "path";
