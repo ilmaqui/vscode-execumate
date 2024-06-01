6bc7c1043bce4d387dd4df9c7f48bcc49926970d
ilmaqui <78042340+ilmaqui@users.noreply.github.com>
2024-06-01 12:53:40 +0200

    Feature and bug fix: Add QuickPickVariables interface and variables property to TerminalNode aswell as fixing a problem where global commands would get deleted after rebooting.

--- src/fileOperations.ts
+++ src/fileOperations.ts
@@ -1,13 +1,30 @@
-import * as fs from "fs/promises";
+import * as fsP from "fs/promises";
+import fs from "fs";
 import { TerminalDataProvider } from "./models/terminal-data-provider";
 import { CommandType } from "./models/command-type";
 import vscode from "vscode";
 import { State } from "./models/state";
 import path from "path";
+import { TerminalNode } from "./models/terminal-node";
+
+export async function createExtensionFolder(context: vscode.ExtensionContext) {
+  const folderPath = context.globalStorageUri.fsPath;
+  const filePath = path.join(folderPath, "global-execumate.json");
+  try {
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+    }
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, "[]");
+    }
+  } catch (error) {
+    console.error("Failed to create extension folder:", error);
+  }
+}
 
 export async function readCommandsFromFile(filePath: string): Promise<any[]> {
   try {
-    const data = await fs.readFile(filePath, "utf8");
+    const data = await fsP.readFile(filePath, "utf8");
     return JSON.parse(data);
   } catch (error) {
     console.error("Failed to read commands from file:", error);
@@ -15,9 +32,62 @@ export async function readCommandsFromFile(filePath: string): Promise<any[]> {
   }
 }
 
+export async function saveCommandToJson(
+  command: string,
+  label: string,
+  isGlobal: boolean,
+  context: vscode.ExtensionContext,
+  variables: string[]
+) {
+  const fileName = isGlobal
+    ? "global-execumate.json"
+    : "workspace-execumate.json";
+  const folderPath = isGlobal
+    ? context.globalStorageUri.fsPath
+    : vscode.workspace.workspaceFolders &&
+      vscode.workspace.workspaceFolders[0].uri.fsPath;
+  if (folderPath) {
+    const filePath = path.join(folderPath, fileName);
+
+    let commands = [];
+    if (fs.existsSync(filePath)) {
+      commands = JSON.parse(fs.readFileSync(filePath, "utf8"));
+    }
+    commands.push({ label, command, variables });
+    fs.writeFileSync(filePath, JSON.stringify(commands));
+  }
+}
+
+export async function saveCommandsToFile(
+  terminals: TerminalNode[],
+  cType: CommandType,
+  context: vscode.ExtensionContext
+) {
+  if (cType !== CommandType.TEMPORARY) {
+    const isGlobal = cType === CommandType.GLOBAL;
+    const filteredTerminals = terminals.filter((c) => c.cType === cType);
+    const fileName = isGlobal
+      ? "global-execumate.json"
+      : "workspace-execumate.json";
+    const folderPath = isGlobal
+      ? context.globalStorageUri.fsPath
+      : vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+    if (folderPath) {
+      const filePath = path.join(folderPath, fileName);
+      const mappedCommands = filteredTerminals.map((t) => ({
+        command: t.command,
+        label: t.label,
+        variables: t.variables,
+      }));
+      fs.writeFileSync(filePath, JSON.stringify(mappedCommands));
+    }
+  }
+}
+
 export async function loadCommandsFromFile(
   provider: TerminalDataProvider,
-  cType: CommandType
+  cType: CommandType,
+  context: vscode.ExtensionContext
 ) {
   if (cType !== CommandType.TEMPORARY) {
     const fileName =
@@ -26,7 +96,7 @@ export async function loadCommandsFromFile(
         : "workspace-execumate.json";
     const folderPath =
       cType === CommandType.GLOBAL
-        ? vscode.extensions.all[0].extensionPath
+        ? context.globalStorageUri.fsPath
         : vscode.workspace.workspaceFolders?.[0].uri.fsPath;
 
     if (folderPath) {
@@ -38,7 +108,8 @@ export async function loadCommandsFromFile(
           State.STOPPED,
           item.label,
           cType,
-          provider
+          provider,
+          item.variables ?? []
         )
       );
     }
